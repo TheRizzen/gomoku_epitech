eef54e7d5cd70563b43e587d41eab042e78ca126
adrien muzika <adrien.muzika@epitech.eu>
2016-10-27 16:24:35 +0200

    game.js beginning :3

--- Game/game.js
+++ Game/game.js
@@ -1,8 +1,14 @@
-var game = {
-  col_nb = 19,
-  line_nb = 19,
-  map = [],
-  players = []
-};
+var game = [];
 
+function addGame() {
+  game.push({
+  init: false,
+  alive: true, // 1 both player are and want playing, 0 -> room is closed
+  gameOngoing: false, // 0 game not ongoing, 1 game still going
+  col_nb: 19,
+  line_nb: 19,
+  map: [],
+  players: []
+  });
+}
 exports = module.exports = game;
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var game = require('./Game/game.js');
-var join = require('./Game/join.js')(io);
+//var join = require('./Game/join.js')(io);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
