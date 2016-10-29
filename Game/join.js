a766c97b73ddb1fc8132a64b95042fe7dfbb3bb1
Thibault Miranda de Oliveira <thibaultmiranda@epitech.eu>
2016-10-29 15:19:37 +0200

    fix join problem

--- Game/join.js
+++ Game/join.js
@@ -3,15 +3,16 @@ Player = require('./player.js')
 
 module.exports = function(io, games) {
 
-  var game = games[games.length - 1];
-
   io.on('connection', function(socket) {
+    var game = games[games.length - 1];
+
     socket.join(game.room);
     game.players.push(new Player(socket.id, socket));
     socket.emit('connected', {user: game.players.length, room: games.length})
     if (game.players.length == 2){
       game.init = true;
       io.to(game.room).emit('starting');
+      games.push(new Game('room #' + games.length));
     }
   });
 };
