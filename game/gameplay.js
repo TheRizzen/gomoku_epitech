b0b174264bbee9b18d2e151dc3e7eeb88eefe57a
Thibault Miranda de Oliveira <thibaultmiranda@epitech.eu>
2016-11-01 18:58:31 +0100

    fix gameplay.sj

--- game/gameplay.js
+++ game/gameplay.js
@@ -3,13 +3,11 @@ var misc = require('./misc.js');
 module.exports = {
   putpawn: function(io, socket, games) {
     var checkgame = function(game, player, x, y) {
-      console.log("Pawn index = " + game.get1DP(x, y));
-
       // Check si pion pris
       var taken = game.arePawnTaken(x, y);
 
       if (taken[0] != -1)
-        io.to(game.room).emit('eatpawn', x1: taken[0], y1: taken[1], x2: taken[2], y2: taken[3]);
+        io.to(game.room).emit('eatpawn', {x1: taken[0], y1: taken[1], x2: taken[2], y2: taken[3], player: player.id});
       // Check de fin de jeu
       if (game.tenPawnTaken() == true) {
         console.log("Partie gagné : 10 pions pris !");
@@ -28,13 +26,10 @@ module.exports = {
       var player = misc.find_player(games, socket.id);
       var game = player.game;
 
-      console.log(game.map[y * game.col_nb + x]);
       if (game.map[y * game.col_nb + x] != 0 || data.player != game.activePlayer + 1) {
-        console.log('badmove !');
         socket.emit('badmove', {x: x, y: y, value: game.map[y * game.col_nb + x]});
       }
       else {
-        console.log('goodmove !');
         game.map[y * game.col_nb + x] = player.id;
         io.to(game.room).emit('move', {x: x, y: y, player: player.id});
 	checkgame(game, player, x, y);
