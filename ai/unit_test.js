7c2e774d1cef30bac642e56df7db414410c1756e
adrien muzika <adrien.muzika@epitech.eu>
2016-11-30 18:29:03 +0100

    better AI placing

--- ai/unit_test.js
+++ ai/unit_test.js
@@ -16,9 +16,9 @@ function testIA() {
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -34,17 +34,23 @@ function testIA() {
 
     this.valueMap = [];
     
-    this.clearBoard = function() {
+    this.clearArray = function(array) {
 	var i = 0;
+	var y = 0;
 	
-        while (i < this.col_nb * this.line_nb)
+        while (i < 19)
         {
-            this.map[i] = 0;
+	    y = 0;
+	    while (y < 19)
+	    {
+		array[i][y] = 0;
+		y += 1;
+	    }
             i += 1;
         }
     }
     
-    this.emptyArray = function(map) {
+    this.emptyMap = function() {
 	var i = 0;
 	var y = 0;
 
@@ -53,7 +59,7 @@ function testIA() {
 	    y = 0;
 	    while (y < 19)
 	    {
-		if (map[i][y] != 0)
+		if (this.map[i][y] != 0)
 		    return false;
 		y += 1;
 	    }
@@ -62,22 +68,30 @@ function testIA() {
 	return true;
     }
     
-    this.assignValue = function(x, y)
+    this.assignValue = function(x, y, map)
     {
 	var i = 0;
 	var value = 0;
+	var xmp = 0;
+	var ymp = 0;
 
 	while (i < 8)
 	{
-	    if (this.map[x + this.moves[i][0]], this.map[y + this.moves[i][1]])
-		value += 1;
+	    if ((x + this.moves[i][0] >= 0) && (y + this.moves[i][1] >= 0) && 
+	       (x + this.moves[i][0] <= 18) && (y + this.moves[i][1] <= 18))
+	    {
+		xmp = x + this.moves[i][0];
+		ymp = y + this.moves[i][1];
+		if (this.map[xmp][ymp] != 0)
+		    value += 1;
+	    }
 	    i += 1;
 	}
 	return value;
     }
-
+    
     this.findPlay = function() {
-	if (this.emptyArray(this.map) == true)
+	if (this.emptyMap() == true)
 	    return ([18 / 2, 18 / 2]);
 
 	var x = 0;
@@ -92,7 +106,7 @@ function testIA() {
 	    while (y < 19)
 	    {
 		tmp = this.assignValue(x, y);
-		if (tmp > maxValue)
+		if (tmp > maxValue && this.map[x][y] == 0)
 		{
 		    maxX = x;
 		    maxY = y;
