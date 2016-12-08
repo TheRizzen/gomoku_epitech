8a0656ba579cf1a7c46359b4a41ec7f8d9790d0b
adrien muzika <adrien.muzika@epitech.eu>
2016-12-08 11:53:41 +0100

    minMax implemented

--- ai/main.js
+++ ai/main.js
@@ -1,6 +1,11 @@
+var now = require("performance-now");
 var testIA = require('./unit_test.js');
 
 test = new testIA();
-var result = test.findPlay();
+var pawn_type = 2;
+var start = now();
+var result = test.findPlay(pawn_type);
+var end = now();
 
 console.log(result);
+console.log("AI execute time : " + (end - start).toFixed(3));
--- ai/unit_test.js
+++ ai/unit_test.js
@@ -94,38 +94,44 @@ function testIA() {
 	return i;
     }
 
-    this.assignValue = function(x, y, map)
+    this.assignValue = function(x, y)
     {
 	var i = 0;
 	var value = 0;
-	var xmp = 0;
-	var ymp = 0;
 
 	while (i < 8)
 	{
 	    if ((x + this.moves[i][0] >= 0) && (y + this.moves[i][1] >= 0) && 
 	       (x + this.moves[i][0] <= 18) && (y + this.moves[i][1] <= 18))
 	    {
-		xmp = x + this.moves[i][0];
-		ymp = y + this.moves[i][1];
-		if (map[xmp][ymp] != 0)
+		if (this.map[x + this.moves[i][0]][y + this.moves[i][1]] != 0)
 		    value += 1;
 	    }
 	    i += 1;
 	}
-	return value;
+	return [x, y, value];
     }
     
     this.determineBestMove = function(resArray, valArray) {
 	return [0, 0];
     }
     
-    this.minMaxLoop = function(depth) {
+/*    this.minMaxLoop = function(depth, supResult, supValue, pawn_type, minMax) {
 	var y = 0;
 	var x = 0;
 	var arrInc = 0;
 	var resultArray = [];
 	var valueArray = [];
+	var dupMap;
+
+	if (supResult[0] != 0 && supResult[1] != 0)
+	{
+	    dupMap = this.map.slice(0);
+	    console.log(supResult);
+	    dupMap[supResult[0]][supResult[1]] == pawn_type;
+	}
+	else
+	    dupMap = this.map;
 
 	if (depth > 0)
 	{
@@ -136,7 +142,7 @@ function testIA() {
 		{
 		    if (this.isTherePawnAround(x, y) == 1)
 		    {
-			valueArray[arrInc] = this.assignValue(x, y, this.map);
+			valueArray[arrInc] = this.assignValue(x, y, dupMap, pawn_type);
 			resultArray[arrInc] = [x, y];
 			arrInc += 1;
 		    }
@@ -144,22 +150,140 @@ function testIA() {
 		}
 		x += 1;
 	    }
+
+	    if (arrInc > 0)
+	    {
+		var tmpInc = arrInc + 1;
+		var recursResult = [];
+		var resultInc = 0;
+
+		while (tmpInc > 0)
+		{
+		    recursResult[resultInc] = this.minMaxLoop(depth - 1, valueArray[tmpInc - 1], supResult[tmpInc - 1], pawn_type == 1 ? 2 : 1, minMax ^ 1);
+		    tmpInc -= 1;
+		    resultInc += 1;
+		}
+		
+	    }
 	}
 
 	console.log(valueArray);
 	console.log(resultArray);
 
-	var result = this.determineBestMove(resultArray, valueArray);
+	var result = this.determineBestMove(resultArray, valueArray, minMax);
 	return result;
-    }
+    }*/
     
-    this.findPlay = function() {
+    this.getMax = function(x, y, pawn_type, depth) {
+	if (depth == 0)
+	    return this.assignValue(x, y, pawn_type);
+
+	var max = -10000;
+	var maxI = 0;
+	var maxJ = 0;
+	var i = 0;
+	var j;
+	var tmp;
+
+	while (i < 19)
+	{
+	    j = 0;
+	    while (j < 19)
+	    {
+		if (this.map[i][j] == 0)
+		{
+		    this.map[i][j] = pawn_type;
+		    tmp = this.getMin(x, y, (pawn_type == 1 ? 2 : 1), depth - 1);
+
+		    if (tmp[2] > max)
+		    {
+			maxI = i;
+			maxj = j;
+			max = tmp[2];
+		    }
+		    this.map[i][j] = 0;
+		}
+		j += 1;
+	    }
+	    i += 1;
+	}
+	return [maxI, maxJ, max];
+    }
+
+    this.getMin = function(x, y, pawn_type, depth) {
+	if (depth == 0)
+	    return this.assignValue(x, y, pawn_type);
+
+	var min = 100000;
+	var maxI = 0;
+	var maxJ = 0;
+	var i = 0;
+	var j;
+	var tmp;
+
+	while (i < 19)
+	{
+	    j = 0;
+	    while (j < 19)
+	    {
+		if (this.map[i][j] == 0)
+		{
+		    this.map[i][j] = pawn_type;
+		    tmp = this.getMax(x, y, (pawn_type == 1 ? 2 : 1), depth - 1);
+
+		    if (tmp[2] < min)
+		    {
+			maxI = i;
+			maxj = j;
+			min = tmp[2];
+		    }
+		    this.map[i][j] = 0;
+		}
+		j += 1;
+	    }
+	    i += 1;
+	}
+	return [maxI, maxJ, min];
+    }
+
+    this.minMaxLoop = function(depth, pawn_type) {
+	var max = -10000;
+	var tmp;
+	var maxX = -1;
+	var maxY = -1;
+	var i = 0;
+	var j;
+
+	while (i < 19)
+	{
+	    j = 0;
+	    while (j < 19)
+	    {
+		if (this.map[i][j] == 0)
+		{
+		    this.map[i][j] = 1;
+		    tmp = this.getMin(i, j, pawn_type, depth - 1);
+		    if (tmp[2] > max)
+		    {
+			max = tmp[2];
+			maxX = i;
+			maxY = j;
+		    }
+		    this.map[i][j] = 0;
+		}
+		j += 1;
+	    }
+	    i += 1;
+	}
+	return [maxX, maxY];
+    }
+
+    this.findPlay = function(pawn_type) {
 	if (this.emptyMap() == true)
 	    return ([18 / 2, 18 / 2]);
 
 	var depth = 2;
-
-	return this.minMaxLoop(depth);
+	return this.minMaxLoop(depth, pawn_type);
     }
 };
 
