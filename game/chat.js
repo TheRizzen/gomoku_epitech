d72bbea677921af302e9be34d07521355c58d75f
alexy.hostetter <alexy.hostetter@epitech.eu>
2016-12-08 14:38:53 +0100

    m

--- game/chat.js
+++ game/chat.js
@@ -2,7 +2,7 @@ var misc = require('./misc.js')
 
 module.exports = function(io, socket, games) {
   socket.on('message', function (data) {
-    game = misc.find_player(games, socket.id).game;
+      game = misc.find_player(games, socket.id).game;
     io.to(game.room).emit('message', {user: data.user, message: data.message});
   });
 };
